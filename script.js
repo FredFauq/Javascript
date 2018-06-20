2a684010d2feea2a0689c4ac0274cb752f5febc7
FredericF <frederic.fauquembert@sfr.fr>
2018-06-20 15:22:09 +0200

    Script Exo 6

---
+++ script.js
@@ -0,0 +1,7 @@
+//Déclaration, demande et assignation des variables
+var firstNumber = prompt("Entrez un premier nombre : ");
+var secondNumber = prompt("Entrez un second nombre : ");
+//Conversion des variables chaînes de caractère en nombre et opération
+result = parseInt(firstNumber) / parseInt(secondNumber);
+//Affichage du résultat
+alert(result);
