bbed1ca0004a82d68bae3e99d6c59574b38ec830
FredericF <frederic.fauquembert@sfr.fr>
2018-06-21 12:39:48 +0200

    script8.js

--- script8.js
+++ script8.js
@@ -2,12 +2,16 @@
 function condition() {
     //declaration des variables
   var age = document.getElementById('age').value;
-  var regex = /^[0-9\.]+$/;
+  var regex1 = /^[0-9]{0,2}$/;
+  var regex2 = /^[a-zA-Z]+$/;
     //Regex et Vérification de la condition de majeur
-  if((regex.test(age) == true) && (age < 18)){
+  if((regex1.test(age) == true) && (age < 18)){
       alert('Vous étes mineur')
     }
-    else{
+    else if(regex2.test(age) == true){
+      alert('Veuillez utiliser des chiffres SVP.')
+    }
+    else {
       alert('Vous étes majeur')
     }
   }
