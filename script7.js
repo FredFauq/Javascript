3fee7685c3d9145f1d369e87b720ac6ebff6672f
FredericF <frederic.fauquembert@sfr.fr>
2018-06-21 14:44:14 +0200

    Script exo 7

--- script7.js
+++ script7.js
@@ -3,13 +3,13 @@ function operation() {
     //declaration des variables
   var shoeSize = document.getElementById('shoeSize').value;
   var yearOfBirth = document.getElementById('yearOfBirth').value;
-  var regex = /^[0-9\.]+$/;
+  var regex = /^[0-9]+$/;
     //Boite de dialogue avec appel des variables
-  if((regex.test(shoeSize) == true) && (regex.test(yearOfBirth) == true)){
+  if(regex.test(shoeSize) && regex.test(yearOfBirth)){
     //Conversion des variables chaînes de caractère en nombre et opération
-    result = (((parseInt(shoeSize) * 2) + 5) * 50) - parseInt(yearOfBirth) + 1766;
+    result = ((shoeSize * 2 + 5) * 50) - yearOfBirth + 1766;
     alert(result);
-  }else{
-  alert('Utiliser seulement des chiffres SVP.');
+  } else {
+  alert('Utilisez seulement des chiffres SVP.');
   }
   }
